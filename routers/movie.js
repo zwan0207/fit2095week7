8281dc09ca3e0b219b3da24c059185c1e16484a3
zwan0207 <zwan0207@student.monash.edu>
2021-09-07 14:23:17 +0800

    initial commit

---
+++ routers/movie.js
@@ -0,0 +1,47 @@
+var Actor = require('../models/actor');
+var Movie = require('../models/movie');
+const mongoose = require('mongoose');
+
+module.exports = {
+
+    getAll: function (req, res) {
+        Movie.find(function (err, movies) {
+            if (err) return res.status(400).json(err);
+
+            res.json(movies);
+        });
+    },
+
+
+    createOne: function (req, res) {
+        let newMovieDetails = req.body;
+        newMovieDetails._id = new mongoose.Types.ObjectId();
+        Movie.create(newMovieDetails, function (err, movie) {
+            if (err) return res.status(400).json(err);
+
+            res.json(movie);
+        });
+    },
+
+
+    getOne: function (req, res) {
+        Movie.findOne({ _id: req.params.id })
+            .populate('actors')
+            .exec(function (err, movie) {
+                if (err) return res.status(400).json(err);
+                if (!movie) return res.status(404).json();
+
+                res.json(movie);
+            });
+    },
+
+
+    updateOne: function (req, res) {
+        Movie.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, movie) {
+            if (err) return res.status(400).json(err);
+            if (!movie) return res.status(404).json();
+
+            res.json(movie);
+        });
+    }
+};
